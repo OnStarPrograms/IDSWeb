79df282676918af3c48374037c2a20f8d913039d
OnStarPrograms <on@starprograms.dev>
2024-06-04 20:50:47 -0400

    done with most of the website

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var Green = -1;
 const Pages = ["Home_Page", "Volunteer_Page", "Stats_Page"];
+const functPage = ["HomePage", "VolunteeringPage", "StatsPage"]
 
 function Switch_Page(Div)
 {
@@ -9,16 +10,19 @@ function Switch_Page(Div)
             if (document.getElementsByClassName(Pages[0])[0].classList.contains("Invisible"))
             {
                 document.getElementsByClassName(Pages[0])[0].classList.toggle("Invisible");
+                
             }
 
             if(!document.getElementsByClassName(Pages[1])[0].classList.contains("Invisible"))
             {
                 document.getElementsByClassName(Pages[1])[0].classList.toggle("Invisible");
+                
             }
 
             if(!document.getElementsByClassName(Pages[2])[0].classList.contains("Invisible"))
             {
                 document.getElementsByClassName(Pages[2])[0].classList.toggle("Invisible");
+                
             }
 
             document.getElementById("VolunteerHeaderText").innerHTML="Home";
@@ -28,17 +32,20 @@ function Switch_Page(Div)
             if (document.getElementsByClassName(Pages[1])[0].classList.contains("Invisible"))
             {
                 document.getElementsByClassName(Pages[1])[0].classList.toggle("Invisible");
+                
             }
 
 
             if(!document.getElementsByClassName(Pages[0])[0].classList.contains("Invisible"))
             {
                 document.getElementsByClassName(Pages[0])[0].classList.toggle("Invisible");
+                
             }
 
             if(!document.getElementsByClassName(Pages[2])[0].classList.contains("Invisible"))
             {
                 document.getElementsByClassName(Pages[2])[0].classList.toggle("Invisible");
+                
             }
 
             document.getElementById("VolunteerHeaderText").innerHTML="Monthly Volunteer Vote";
@@ -48,16 +55,19 @@ function Switch_Page(Div)
             if (document.getElementsByClassName(Pages[2])[0].classList.contains("Invisible"))
             {
                 document.getElementsByClassName(Pages[2])[0].classList.toggle("Invisible");
+                
             }
 
             if(!document.getElementsByClassName(Pages[0])[0].classList.contains("Invisible"))
             {
                 document.getElementsByClassName(Pages[0])[0].classList.toggle("Invisible");
+                document.getElementById(functPage[0]).classList.toggle("Active");
             }
 
             if(!document.getElementsByClassName(Pages[1])[0].classList.contains("Invisible"))
             {
                 document.getElementsByClassName(Pages[1])[0].classList.toggle("Invisible");
+                // document.getElementById(functPage[1]).classList.toggle("Active");
             }
 
             document.getElementById("VolunteerHeaderText").innerHTML="Department Statistics";
@@ -77,4 +87,10 @@ function toggleGreen(Div)
     }
     Green = Div;
     document.getElementsByClassName("Volunteer")[Green].classList.toggle("Green");
+}
+
+function AlertFunction()
+{
+    alert("Next Volunteering Oppurtunity\n\nThank you for checking out this website!\nHope you enjoy Catalyst!\nJune 14, 2024 @ 2:00 p.m. - 4:30 p.m\n\n Reminder to drink some water :D\n -A.D.T")
+    document.getElementById("VolOverLay").classList.toggle("Invisible");
 }
